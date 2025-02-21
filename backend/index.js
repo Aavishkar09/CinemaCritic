14ee044469e551879c588dcda225fd61b4fddf03
Aavishkar09 <baraskaraavishkar@gmail.com>
2025-02-21 14:41:21 +0530

    Removed CORS URL

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 4001;
 
 // Middleware
 app.use(express.json());
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
 // Connect to Database
